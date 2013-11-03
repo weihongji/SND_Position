8f8c7a288c54a2432bda705e5c4affa7bbcce3cd
Jesse <weihongji+git@gmail.com>
2013-11-03 22:26:47 +0800

    Added the Edit Dialog at draft level.

--- Src/Position/Map/Controllers/HomeController.cs
+++ Src/Position/Map/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Map.Models;
+
 namespace Map.Controllers
 {
     public class HomeController : Controller
@@ -11,10 +13,34 @@ namespace Map.Controllers
         //
         // GET: /Home/
 
-        public ActionResult Index()
-        {
-            return View();
+        public ActionResult Index() {
+            var model = new MapMgmtModel();
+            model.FengYaList.Add(new FengYa {
+                Id = 101,
+                Name = "风压机01",
+                Power = (decimal)5.5,
+                X = 146,
+                Y = 333
+            });
+            model.FengYaList.Add(new FengYa {
+                Id = 102,
+                Name = "风压机02",
+                Power = (decimal)6.5,
+                X = 494,
+                Y = 237
+            });
+            model.FengYaList.Add(new FengYa {
+                Id = 103,
+                Name = "风压机03",
+                Power = (decimal)5.5,
+                X = 630,
+                Y = 199
+            });
+            return View(model);
         }
 
+        public ActionResult GetPins() {
+            return Json(new FengYa(), JsonRequestBehavior.AllowGet);
+        }
     }
 }
---
+++ Src/Position/Map/Models/MapMgmtModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Map.Models
+{
+    public class MapMgmtModel
+    {
+        public List<FengYa> FengYaList { get; set; }
+
+        public MapMgmtModel() {
+            FengYaList = new List<FengYa>();
+        }
+    }
+}
